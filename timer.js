8d159395c067602e788b2517212ddd7f480c470d
Oleksandra-Mas <sashamasyuk0702@gmail.com>
2021-10-06 20:39:09 +0300

    Update timer.js

--- timer.js
+++ timer.js
@@ -5,40 +5,44 @@ class CountdownTimer {
   constructor({ selector, targetDate }) {
     this.targetDate = targetDate;
     this.intervalId;
-      this.timeComponentsRef = document.querySelectorAll(`${selector} .value`);
-      this.init();
+    this.timeComponentsRef = document.querySelectorAll(`${selector} .value`);
+    this.init();
   }
   countTimeComponents(curDate) {
-    const days = this.pad(Math.floor(
-      (this.targetDate - curDate) / (1000 * 60 * 60 * 24)
-    ));
-    const hours =this.pad( Math.floor(
-      ((this.targetDate - curDate) % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    ));
-    const mins =this.pad( Math.floor(
-      ((this.targetDate - curDate) % (1000 * 60 * 60)) / (1000 * 60)
-    ));
-    const secs =this.pad( Math.floor(((this.targetDate - curDate) % (1000 * 60)) / 1000));
+    const days = this.pad(
+      Math.floor((this.targetDate - curDate) / (1000 * 60 * 60 * 24))
+    );
+    const hours = this.pad(
+      Math.floor(
+        ((this.targetDate - curDate) % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      )
+    );
+    const mins = this.pad(
+      Math.floor(((this.targetDate - curDate) % (1000 * 60 * 60)) / (1000 * 60))
+    );
+    const secs = this.pad(
+      Math.floor(((this.targetDate - curDate) % (1000 * 60)) / 1000)
+    );
     return [days, hours, mins, secs];
   }
   init() {
     this.intervalId = setInterval(() => {
       const currentDate = Date.now();
-        const countdownDate = this.countTimeComponents(currentDate);
-        if (countdownDate.every(timeEl => timeEl < 0)) {
-            clearInterval(this.intervalId);
-            return;
-        }
-        this.updateTimer(countdownDate);
+      const countdownDate = this.countTimeComponents(currentDate);
+      if (countdownDate.every((timeEl) => timeEl < 0)) {
+        clearInterval(this.intervalId);
+        return;
+      }
+      this.updateTimer(countdownDate);
     }, 1000);
   }
   updateTimer(countdownDate) {
     [...this.timeComponentsRef].reduce(function (prevValue, curValue, index) {
       curValue.textContent = countdownDate[index];
     }, this.timeComponentsRef[0]);
-    }
-    pad(value) {
-    return String(value).padStart(2, '0');
+  }
+  pad(value) {
+    return String(value).padStart(2, "0");
   }
 }
 const timer = new CountdownTimer({
